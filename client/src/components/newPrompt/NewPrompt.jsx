607cc161d65ece30fb6a22ef94b393c50fb83e40
PatientPadawan <49569154+PatientPadawan@users.noreply.github.com>
2024-08-02 15:50:57 -0700

    api shell complete

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,24 +1,95 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: "",
+    dbData: {},
+    aiData: {},
+  });
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello, I have 2 dogs in my house." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig: {
+      // maxOutputTokens: 100,
+    },
+  });
+
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  });
+  }, [question, answer, img.dbData]);
+
+  const add = async (prompt) => {
+    setQuestion(prompt);
+
+    const result = await chat.sendMessageStream(
+      Object.entries(img.aiData).length ? [img.aiData, prompt] : [prompt]
+    );
+    let responseText = "";
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      console.log(chunkText);
+      responseText += chunkText;
+      setAnswer(responseText);
+    }
+    setImg({ isLoading: false, error: "", dbData: {}, aiData: {} });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const prompt = e.target.prompt.value;
+    if (!prompt) return;
+
+    add(prompt);
+  };
 
   return (
     <>
       {/* ADD NEW CHAT */}
-      TEST
-      <div className="end-chat" ref={endRef}></div>
-      <form className="new-form">
-        <label htmlFor="file">
-          <img src="/attachment.png" alt="attachment button" />
-        </label>
+      {img.isLoading && <div className="">Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width={380}
+          transformation={[{ width: 380 }]}
+        />
+      )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
+      <div className="end-chat" ref={endRef} />
+      <form className="new-form" onSubmit={handleSubmit}>
+        <Upload setImg={setImg} />
         <input id="file" type="file" multiple={false} hidden />
-        <input type="text" placeholder="Ask me anything..." />
+        <input
+          id="new-prompt"
+          name="prompt"
+          type="text"
+          placeholder="Ask me anything..."
+        />
         <button>
           <img src="/arrow.png" alt="send message arrow" />
         </button>
